fcc8eb74b36e2c6acb4742e608ffb2c66a4a976e
Vansh <vanshgarg32@gmail.com>
2023-01-13 16:01:48 +0530

    code

---
+++ app/src/main/java/com/example/compound_vinhack/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.compound_vinhack;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView deny = (TextView) findViewById(R.id.deny);
+        deny.setPaintFlags(deny.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+    }
+}
